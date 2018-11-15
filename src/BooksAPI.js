0a76b14e3d594ea17f7fb6a390a8cb2c9d80f3fb
Cemile B <cbiyiktay@zacks.com>
2018-11-15 13:09:54 -0600

    Book & Shelf components created

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -5,6 +5,8 @@ const api = "https://reactnd-books-api.udacity.com"
 // Generate a unique token for storing your bookshelf data on the backend server.
 let token = localStorage.token
 if (!token)
+/* - Generates random string, stores it in local storage and then authorization uses the token
+   - The server knows what information to send back based on the token */
   token = localStorage.token = Math.random().toString(36).substr(-8)
 
 const headers = {
@@ -12,16 +14,19 @@ const headers = {
   'Authorization': token
 }
 
+/* Get information about a single book. Returns only one book */
 export const get = (bookId) =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then(res => res.json())
     .then(data => data.book)
 
+/* Returns all the books that we have on all bookshelves */
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
     .then(res => res.json())
     .then(data => data.books)
 
+/* Updates a single book and its shelf when we move a book to a new shelf */
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
     method: 'PUT',
@@ -32,6 +37,7 @@ export const update = (book, shelf) =>
     body: JSON.stringify({ shelf })
   }).then(res => res.json())
 
+/* Searches for books using the parameter query which posts to this endpoint and returns books */
 export const search = (query) =>
   fetch(`${api}/search`, {
     method: 'POST',
