527236205c77ff10d518a69e3ede54c3941b98a5
jamieleegtb <cemilegtaman@gmail.com>
2018-11-26 22:20:43 -0600

    Correct shelf highlighted on searched books

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -3,7 +3,19 @@ import {Link} from 'react-router-dom';
 import {search} from '../../BooksAPI';
 import Book from '../Book';
 
+import {getAll} from '../../BooksAPI';
+
 export default class Search extends Component {
+  /* We will get all the books */
+  async componentDidMount(){
+    try {
+      const books = await getAll();
+      this.props.addBooks(books);
+
+    } catch(error){
+      console.log(error);
+    }
+  }
   /* We'll also set the value for the change */
   constructor(props) {
     super(props);
@@ -50,9 +62,20 @@ export default class Search extends Component {
         <div className="search-books-results">
           <ol className="books-grid">
             {/* Will show the searched books */}
-            {this.state.books.length > 0 && this.state.books.map(book => (
-              <Book key={book.id} {...book} moveBook={this.props.moveBook} />
-            ))}
+            {/* Match the corresponding shelf with the searched book */}
+            {this.state.books.length > 0 &&
+              this.state.books.map(book => {
+                const foundShelf = this.props.books.find(
+                  searchBook => searchBook.id === book.id
+                );
+                if (foundShelf){
+                  book.shelf = foundShelf.shelf;
+                } else {
+                  book.shelf = "none";
+                }
+              return (<Book key={book.id} {...book} moveBook={this.props.moveBook} />);
+              }
+            )}
           </ol>
         </div>
       </div>
